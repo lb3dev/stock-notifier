da5672c7c0b0e9242c95e3901f3b92409c42ac06
lb3dev <lben3dev@gmail.com>
2023-02-23 19:16:10 -0500

    Adding multiple color select support for Uniqlo

--- src/uniqlo.js
+++ src/uniqlo.js
@@ -3,33 +3,49 @@ const { chromium } = require('playwright');
 
 class Uniqlo extends Base {
 
-    constructor(url, threshold) {
+    constructor(url, threshold, colors) {
         super();
         this.url = url;
         this.threshold = threshold;
+        this.colors = colors;
     }
 
-    async check() {
+    async checkColor(color) {
+        if (color) {
+            await this.page.locator(`dt.color >> input[value="${color}"] + label`).click();
+            this.url = await this.page.url();
+        }
+
         const titleElem = await this.page.locator('#right >> div[data-test="product-name"] >> span.title');
         this.title = await titleElem.innerText();
 
         const addToCart = await this.page.locator('#right >> button[data-test="add-to-cart-button"]');
         this.inStock = (await addToCart.getAttribute('disabled')) === null;
 
-        if (this.inStock) {
-            this.onSale = await this.page.locator('#right >> span.price-limited').isVisible();
-            if (this.onSale) {
-                const salePrice = await this.page.locator('#right >> span.price-limited >> span.fr-price-currency >> span');
-                this.price = parseFloat(await salePrice.innerText());
-            } else {
-                const regularPrice = await this.page.locator('#right >> span.price-original >> span.fr-price-currency >> span');
-                this.price = parseFloat(await regularPrice.innerText());
-            }
-
-            this.hit = this.price <= this.threshold;
+        this.onSale = await this.page.locator('#right >> span.price-limited').isVisible();
+        if (this.onSale) {
+            const salePrice = await this.page.locator('#right >> span.price-limited >> span.fr-price-currency >> span');
+            this.price = parseFloat(await salePrice.innerText());
+        } else {
+            const regularPrice = await this.page.locator('#right >> span.price-original >> span.fr-price-currency >> span');
+            this.price = parseFloat(await regularPrice.innerText());
         }
 
+        if (this.inStock) {
+            this.hit = this.price <= this.threshold;
+        }
         this.row();
+    }
+
+    async check() {
+        await this.overlay();
+        if (this.colors) {
+            for (const color of this.colors) {
+                await this.checkColor(color);
+            }
+        } else {
+            await this.checkColor(null);
+        }
     }
 
     async overlay() {
@@ -45,7 +61,7 @@ class Uniqlo extends Base {
 }
 
 async function run(config) {
-    return await new Uniqlo(config.url, config.threshold).run();
+    return await new Uniqlo(config.url, config.threshold, config.colors).run();
 }
 
 module.exports = {
