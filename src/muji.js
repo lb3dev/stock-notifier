1b4b306511dec6046e8f8d90a03778f70fd08b87
lb3dev <lben3dev@gmail.com>
2023-04-30 19:27:12 -0400

    Added logic to muji module to detect online only sales

--- src/muji.js
+++ src/muji.js
@@ -13,9 +13,15 @@ class Muji extends Base {
         const title = await this.page.locator('h1.product-title');
         this.title = await title.innerText();
 
-        const price = await this.page.locator('div.price__current >> span.money').innerText();
+        let price = await this.page.locator('div.price__current >> span.money').innerText();
         this.onSale = (await this.page.locator('span.product__badge--sale').count()) > 0;
+        const onlineSale = (await this.page.locator('div.tdf_discountable_product').count()) > 0;
         this.price = parseFloat(price.replace('$', '').trim());
+
+        if (onlineSale) {
+            price = await this.page.locator('div.tdf_discountable_product >> span.tdf_price_sales >> span.money').innerText();
+            this.price = parseFloat(price.replace('$', '').trim());
+        }
 
         const selector = await this.page.locator('select.options-selection__input-select');
         const sizes = await selector.locator('option').all();
